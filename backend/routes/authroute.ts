ece8bd1eada6be52a8fabade0153069ae6716afb
anurag <yourboyanurag777@gmail.com>
2026-04-02 12:53:11 +0530

    Added routes

--- backend/routes/authroute.ts
+++ backend/routes/authroute.ts
@@ -1,12 +1,17 @@
 
 import { Register } from "../controller/auth.ts";
 import {login} from "../controller/auth.ts"
+import { getuser } from "../controller/auth.ts";
+import { getalluser } from "../controller/auth.ts";
 import { Router } from "express";
 
 
 
 const Authrouter = Router()
 Authrouter.post('/register', Register);
-Authrouter.post('/login', login)
+Authrouter.post('/login', login);
+Authrouter.get('/get-user', getuser);
+Authrouter.get('/get-all-user', getalluser);
+
 
 export default Authrouter;
